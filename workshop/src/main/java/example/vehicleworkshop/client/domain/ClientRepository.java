29f61fec917a43e0a62825ffd00ce9e226749464
Kamil Jędrzejuk <kjedrzejuk@9bits.pl>
2018-02-01 19:37:46 +0100

    Refaktor usuniecię zbędnych metod w interfejsie.

--- workshop/src/main/java/example/vehicleworkshop/client/domain/ClientRepository.java
+++ workshop/src/main/java/example/vehicleworkshop/client/domain/ClientRepository.java
@@ -7,11 +7,7 @@ import java.util.Optional;
 
 interface ClientRepository extends DomainRepository<Client, BaseAggregateRoot.AggregateId> {
 
-    void save(Client client);
-
     Client findByPersonalNumberOrThrow(String personalNumber);
 
     Optional<Client> findByPersonalNumber(String personalNumber);
-
-    void delete(Client client);
 }
